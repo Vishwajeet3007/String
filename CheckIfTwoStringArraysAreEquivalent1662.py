9f3dbe29439c5a99b6721830dcf510cafe6b7f7a
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-12 09:16:24 +0530

    Update CheckIfTwoStringArraysAreEquivalent1662.py

--- CheckIfTwoStringArraysAreEquivalent1662.py
+++ CheckIfTwoStringArraysAreEquivalent1662.py
@@ -1,5 +1,20 @@
 def arrayStringAreEqual(word1,word2):
-    return "".join(word1) == "".join(word2)
+    #return "".join(word1) == "".join(word2)
+    i = j = 0
+    p1 = p2 = 0
+    while i < len(word1) and j < len(word2):
+        if word1[i][p1] != word2[j][p2]:
+            return False
+        p1 += 1
+        p2 += 1
+
+        if p1 == len(word1[i]):
+            p1 = 0
+            i += 1
+        if p2 == len(word2[j]):
+            p2 = 0
+            j += 1
+    return i == len(word1) and j == len(word2)
 word1 = ["ab", "c"]
 word2 = ["a", "bc"]
 print(arrayStringAreEqual(word1,word2))
