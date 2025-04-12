185e30c21c76cf19c07b8626939701789eade557
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-04-12 09:37:00 +0530

    add

---
+++ ConvertStringToLowerCase.py
@@ -0,0 +1,8 @@
+def convertStringToLowerCase(s):
+    return s.lower()
+s = "ABCddE"
+print(convertStringToLowerCase(s))
+s = 'VISHWAJEET'
+print(convertStringToLowerCase(s))
+s = 'VISHWAJEET KUMAR'
+print(convertStringToLowerCase(s))
---
+++ LowerCaseToUpperCase.py
@@ -0,0 +1,6 @@
+def lowerCaseToUpperCase(s):
+    return s.upper()
+s = 'adsvdvd'
+print(lowerCaseToUpperCase(s))
+s = 'vishwajeet'
+print(lowerCaseToUpperCase(s)) 
